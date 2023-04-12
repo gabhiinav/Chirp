90657f2d931e1bfb2d6ff027236b3534b1459cf4
gabhiinav <gabhiinav@gmail.com>
2023-04-13 00:42:27 +0530

    Add Comments

---
+++ app/post/[slug]/page.tsx
@@ -0,0 +1,64 @@
+'use client'
+
+import Post from "../../Post"
+import { useQuery } from "@tanstack/react-query"
+import axios from "axios"
+import AddComment from "../../AddComment"
+import { PostType } from "../../types/Post"
+import Image from "next/image"
+import { motion } from "framer-motion"
+
+const fetchDetails = async (slug: string) => {
+    const response = await axios.get(`/api/posts/${slug}`)
+    return response.data
+}
+
+type URL = {
+    params: {
+      slug: string
+    }
+    searchParams: string
+}
+
+export default function PostDetail(url: URL) {
+    const { data, isLoading } = useQuery<PostType>({
+      queryKey: ["detail-post"],
+      queryFn: () => fetchDetails(url.params.slug),
+    })
+    if (isLoading) return "Loading"
+    console.log(data)
+    return (
+      <div>
+        <Post
+          id={data?.id}
+          name={data?.user.name}
+          avatar={data?.user.image}
+          postTitle={data?.title}
+          comments={data?.comments}
+        />
+        <AddComment id={data?.id} />
+        {data?.comments?.map((comment) => (
+          <motion.div
+            animate={{ opacity: 1, scale: 1 }}
+            initial={{ opacity: 0, scale: 0.8 }}
+            transition={{ ease: "easeOut" }}
+            className="my-6 bg-white p-8 rounded-md"
+            key={comment.id}
+          >
+            <div className="flex items-center gap-2">
+              <Image
+                className="rounded-full"
+                width={24}
+                height={24}
+                src={comment.user?.image}
+                alt="avatar"
+              />
+              <h3 className="font-bold">{comment?.user?.name}</h3>
+              <h2 className="text-sm">{comment.createdAt}</h2>
+            </div>
+            <div className="py-4">{comment.title}</div>
+          </motion.div>
+        ))}
+      </div>
+    )
+  }
