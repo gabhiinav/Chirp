52b16c566ac9935d1fd562995f32743442d9e420
gabhiinav <gabhiinav@gmail.com>
2023-04-22 01:15:13 +0530

    TS Error

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
       <body className='mx-4 md:mx-48 xl:mx-96 bg-gray-200'>
         <QueryWrapper>
           <AuthContext>
+            {/* @ts-expect-error */}
             <Nav />
             {children}
           </AuthContext>
