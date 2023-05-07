6b4fe96047c5e7725d9bccd938ce47033509cacb
gabhiinav <gabhiinav@gmail.com>
2023-05-08 01:30:39 +0530

    Fixed TypeError

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,37 @@
-"use client"
+"use client";
 
-import CreatePost from "./CreatePost"
-import Post from "./Post"
-import { useQuery } from "@tanstack/react-query"
-import { PostsType } from "./types/Posts"
+import CreatePost from "./CreatePost";
+import Post from "./Post";
+import { useQuery } from "@tanstack/react-query";
+import { PostsType } from "./types/Posts";
 
 const allPosts = async () => {
-  const response = await fetch("/api/posts/homePosts")
-  return response.json()
-}
+  const response = await fetch("/api/posts/homePosts");
+  return response.json();
+};
 
 export default function Home() {
   const { data, error, isLoading } = useQuery<PostsType[]>({
     queryFn: allPosts,
     queryKey: ["posts"],
-  })
-  if (error) return error
-  if (isLoading) return "Loading..."
+  });
+  if (error) return error;
+  if (isLoading) return "Loading...";
 
   return (
     <div>
       <CreatePost />
-      {data?.map((post) => (
-        <Post
-          key={post.id}
-          id={post.id}
-          name={post.user.name}
-          avatar={post.user.image}
-          postTitle={post.title}
-          comments={post.comments}
-        />
-      ))}
+      {data &&
+        data.map((post) => (
+          <Post
+            key={post.id}
+            id={post.id}
+            name={post.user.name}
+            avatar={post.user.image}
+            postTitle={post.title}
+            comments={post.comments}
+          />
+        ))}
     </div>
-  )
+  );
 }
